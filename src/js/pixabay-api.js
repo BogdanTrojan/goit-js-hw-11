0260426633f91f24775ad9a2bf97dda01dbb9c8e
BogdanTrojan <156309243+BogdanTrojan@users.noreply.github.com>
2024-07-10 16:36:22 +0000

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,19 @@
 export function fetchPhotos(input) {
-    const params = new URLSearchParams({
-      key: '44576070-519b0fb3235ae96b7cd44aad7',
-      q: input,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    });
-    const url = `https://pixabay.com/api/?${params}`;
-  
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .catch(error => console.log('Error:', error));
-  }
+  const params = new URLSearchParams({
+    key: '44872956-45ef79406a920b03fc2627c03',
+    q: input,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `https://pixabay.com/api/?${params}`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log('Error:', error));
+}
